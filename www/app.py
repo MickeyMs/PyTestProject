840806d2e5b5f309f72b39f9e41035dea49ae0b9
ZhihaoWang <wzhh512000@gmail.com>
2017-09-19 07:23:06 +0800

    day3 content added and fun passed.

--- www/app.py
+++ www/app.py
@@ -1,4 +1,10 @@
+#! /usr/bin/evn python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ZhihaoWang'
+
 import logging; logging.basicConfig(level= logging.INFO)
+
 import asyncio, os, json, time
 from datetime import datetime
 
@@ -18,3 +24,142 @@ def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host','localhost'),
+        port=kw.get('post', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset','utf8'),
+        autocommit=kw.get('autocommit',True),#自动提交事务
+        maxsize=kw.get('maxsize',10),#池中最多有10个链接对象
+        minisize=kw.get('minisize',1),
+        loop=loop
+    )
+
+# 封装SELECT方法
+@asyncio.coroutine
+def select(sql,args,size=None): #size可以决定取几条
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur=yield from conn.cursor(aiomysql.DictCursor)
+        #用参数替换而非字符串拼接可以防止sql注入
+        yield from cur.execute(sql.replace('?','%s'), args or ())
+        if size:
+            rs=yield from cur.fetchmany(size)
+        else:
+            rs=yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+@asyncioco.coroutine
+def execute(sql, args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+from orm import Model, StringField, IntegerField
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getatrr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s:%s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        'find object by primary key.'
+        rs = yield from select ('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    @asyncio.coroutine
+    def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affeted rows: %s' % rows)
+
+
+class Field(object):
+
+    def __init(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        #排除Model类本身:
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        #获取table名称：
+        tableName = attrs.get('__table__', None) or name
+        logging.info ('found model: %s (table:%s)' % (name, tableName))
+        #获取所有的Field和主键名：
+        mappings = dict()
+        fileds = []
+        primarykey = None
+        for k, v in attr.items():
+            if isinstance(v, Field):
+                logging.info(' found mapping: %s ==> %s' % (k,v))
+                mappings[k] = v
+                if v.primary_key:
+                    #找到主键:
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    field.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_filed = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings #保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey #逐渐属性名
+        #构造默认的SELECT， INSERT，UPDATE和DELETE语句:
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fileds), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fileds), primarKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ','.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f), fileds)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
