1080fb3f9eab98b0d1d6fa6deac6bc4d18ed9d27
ZhihaoWang <wzhh512000@gmail.com>
2017-12-26 06:55:28 +0800

    替换中文全角句号为英文的半角句点

--- www/apis.py
+++ www/apis.py
@@ -11,11 +11,12 @@ import json, logging, inspect, functools
 
 class Page(object):
     '''
-    Page object for display pages。
+    Page object for display pages.
     '''
+
     def __init__(self, item_count, page_index=1, page_size=10):
         '''
-        Init Pagination by item_count, page_index and page_size。
+        Init Pagination by item_count, page_index and page_size.
 
         >>> p1 = Page(100, 1)
         >>> p1.page_count
@@ -55,7 +56,7 @@ class Page(object):
 
     def __str__(self):
         return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' % (self.item_count, self.page_count, self.page_index, self.page_size, self.offset, self.limit)
-    
+
     __repr__ = __str__
 
 class APIError(Exception):
