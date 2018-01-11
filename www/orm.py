8a34ef5d72c35fafbcb3b55a34cd7906582cfec8
ZhihaoWang <wzhh512000@gmail.com>
2018-01-11 23:40:41 +0800

    修改部分参数，添加部分注释

--- www/orm.py
+++ www/orm.py
@@ -7,9 +7,13 @@ import asyncio, logging
 
 import aiomysql
 
+# SQL日志输出
 def log(sql, args=()):
-    logging.info('SQL: %s' % sql)
+    # logging.info('SQL: %s' % sql)
+    sqllog = ('SQL: %s' % sql).replace('?', '%s');
+    logging.info(sqllog % tuple(args or ()))
 
+# 创建全局连接池
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
@@ -26,6 +30,14 @@ async def create_pool(loop, **kw):
         loop=loop
     )
 
+# 销毁连接池
+async def destroy_pool():
+    global __pool
+    if __pool is not None:
+        __pool.close()
+        await __pool.wait_closed()
+
+# SELECTy语句
 async def select(sql, args, size=None):
     log(sql, args)
     global __pool
@@ -39,8 +51,9 @@ async def select(sql, args, size=None):
         logging.info('rows returned: %s' % len(rs))
         return rs
 
+# INSERT、UPDATE、DELETE语句
 async def execute(sql, args, autocommit=True):
-    log(sql)
+    log(sql, args)
     async with __pool.get() as conn:
         if not autocommit:
             await conn.begin()
@@ -53,15 +66,17 @@ async def execute(sql, args, autocommit=True):
         except BaseException as e:
             if not autocommit:
                 await conn.rollback()
-            raise
+            raise e
         return affected
 
+# 构建inssert语句点位符
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ', '.join(L)
 
+# Field和各种Field子类
 class Field(object):
 
     def __init__(self, name, column_type, primary_key, default):
@@ -73,38 +88,47 @@ class Field(object):
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
+# 映射字符串的StringField
 class StringField(Field):
 
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
+# 映射布尔的BooleanField
 class BooleanField(Field):
 
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
+# 映射整型的IntegerField
 class IntegerField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
 
+# 映射浮点型的FloatField
 class FloatField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
+# 映射文本域的TextField
 class TextField(Field):
 
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
+# 读取映射信息的ModelMetaclass
 class ModelMetaclass(type):
 
     def __new__(cls, name, bases, attrs):
+        # 排除Model类本身
         if name=='Model':
             return type.__new__(cls, name, bases, attrs)
+        # 获取table名称
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
+        # 获取所有的Field和主键名
         mappings = dict()
         fields = []
         primaryKey = None
@@ -115,12 +139,12 @@ class ModelMetaclass(type):
                 if v.primary_key:
                     # 找到主键:
                     if primaryKey:
-                        raise StandardError('Duplicate primary key for field: %s' % k)
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
                     primaryKey = k
                 else:
                     fields.append(k)
         if not primaryKey:
-            raise StandardError('Primary key not found.')
+            raise RuntimeError('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
@@ -128,12 +152,18 @@ class ModelMetaclass(type):
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey # 主键属性名
         attrs['__fields__'] = fields # 除主键外的属性名
+        # 构造默认的CRUD操作
+        selectParams = ','.join(escaped_fields)
+        insertParams = ','.join(escaped_fields)
+        insertValues = create_args_string(len(escaped_fields)+1)
+        updateParamsAndValues = ','.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f),fields))
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields)+1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
+# 定义所有ORM映射基类
 class Model(dict, metaclass=ModelMetaclass):
 
     def __init__(self, **kw):
@@ -142,7 +172,7 @@ class Model(dict, metaclass=ModelMetaclass):
     def __getattr__(self, key):
         try:
             return self[key]
-        except KeyError:
+        except KeyError as e:
             raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
     def __setattr__(self,key, value):
@@ -160,7 +190,7 @@ class Model(dict, metaclass=ModelMetaclass):
                 logging.debug('using default value for %s: %s' % (key, str(value)))
                 setattr(self, key, value)
         return value
-
+    # 根据为条件查找、附带排序字段、条数
     @classmethod
     async def findAll(cls, where=None, args=None, **kw):
         ' find objects by where clause. '
@@ -189,6 +219,7 @@ class Model(dict, metaclass=ModelMetaclass):
         return [cls(**r) for r in rs]
 
     @classmethod
+    # 查找某字段
     async def findNumber(cls, selectField, where=None, args=None):
         ' find number by select and where. '
         sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
@@ -199,7 +230,8 @@ class Model(dict, metaclass=ModelMetaclass):
         if len(rs) == 0:
             return None
         return rs[0]['_num_']
-
+    
+    # 往Model类添加class方法， 就可以让所有子类调用class方法
     @classmethod
     async def find(cls, pk):
         ' find object by primary key. '
@@ -207,7 +239,8 @@ class Model(dict, metaclass=ModelMetaclass):
         if len(rs) == 0:
             return None
         return cls(**rs[0])
-
+    
+    # 往Model类添加实例方法， 就可以让所有子类调用示例方法
     async def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primary_key__))
@@ -215,6 +248,7 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to insert record: affected rows: %s' % rows)
 
+    # update处理
     async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primary_key__))
@@ -222,6 +256,7 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to update by primary key: affected rows: %s' % rows)
 
+    # 删除处理
     async def remove(self):
         args = [self.getValue(self.__primary_key__)]
         rows = await execute(self.__delete__, args)
